322425ed42c6d2e07b83e948b1e517274d2f6888
amir0-cell <amirsid950@gmail.com>
2026-01-18 03:42:01 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,18 +1,2 @@
-const express = require('express');
-const app = express();
-
-const ADMIN_TOKEN = "besadgojsdg4ghJLDSJOQUULJSDLGJDldskjghare"; 
-
-app.get('/user-data', (req, res) => {
-    const userToken = req.headers.authorization;
-
-    if (userToken === ADMIN_TOKEN) {
-        const userData = { name: "John", password: "SaYKam123", ssn: "123-45-678" };
-        res.status(200).send(userData); 
-    } else {
-        res.send("Unauthorized");
-        res.json({ error: "Access Denied" }); 
-    }
-});
-
-app.listen(3000);
+cosnt b = 12;
+const b = 13;
