fb7e8a4391f43f086d0ff0f42997f5e97a826ba3
amir0-cell <amirsid950@gmail.com>
2026-01-18 03:52:53 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,3 @@
-const express = require('express');
-const app = express();
-
-const ADMIN_TOKEN = "beareaslkhg3wa($%^&KDare"; 
-
-app.get('/user-data', (req, res) => {
-    const userToken = req.headers.authorization;
-
-    if (userToken === ADMIN_TOKEN) {
-        const userData = { name: "John", password: "John123!@#", ssn: "123-45-678" };
-        res.status(200).send(userData); 
-    } 
-});
-
-app.listen(3000);
+const a = 400;
+const a = 500;
+console.log(a);
