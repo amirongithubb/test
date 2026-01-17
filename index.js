6d93595970c85afeb582a97b14c56cde3f2aabba
amir0-cell <amirsid950@gmail.com>
2026-01-18 03:31:29 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const app = express();
 
-const ADMIN_TOKEN = "besadgoj(&(UJLDSJOQUULJSDLGJDldskjghare"; 
+const ADMIN_TOKEN = "besadgojsdg4ghJLDSJOQUULJSDLGJDldskjghare"; 
 
 app.get('/user-data', (req, res) => {
     const userToken = req.headers.authorization;
 
     if (userToken === ADMIN_TOKEN) {
-        const userData = { name: "John", password: "hashed_secret", ssn: "123-45-678" };
+        const userData = { name: "John", password: "SaYKam123", ssn: "123-45-678" };
         res.status(200).send(userData); 
     } else {
         res.send("Unauthorized");
